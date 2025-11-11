cc3cd00553d9e26444535b693c9972ba090c44b6
Dima Dauzhuk <dima.dauzhuk@icloud.com>
2025-11-11 20:18:53 +0100

    Implemented Task model and admin

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+    
+class Task(models.Model):
+    class Status(models.TextChoices):
+            COMPLETED = 'C', 'Completed'
+            PENDING = 'P', 'Pending'
+            
+    title = models.CharField(max_length = 200)
+    body = models.TextField(null = True, blank = True)
+    published = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length = 1,
+        choices = Status.choices,
+        default = Status.PENDING
+    )
+    changed = models.DateTimeField(auto_now = True)
+    
+    def __str__(self):
+        return self.title
