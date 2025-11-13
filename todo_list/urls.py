690bbecf615517130780564267d4e752fe2a48bd
Dima Dauzhuk <dima.dauzhuk@icloud.com>
2025-11-13 20:58:04 +0100

    Add form to create tasks

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import task_list, task_detail
+from .views import task_list, task_detail, task_create
 
 app_name = 'todo_list'
 
 urlpatterns = [
     path('', task_list, name='task_list'),
-    path('tasks/<int:id>/', task_detail, name='task_detail')
+    path('tasks/<int:id>/', task_detail, name='task_detail'),
+    path('tasks/create/', task_create, name = 'task_create'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Task
+from .forms import TaskCreateForm
+from django.shortcuts import redirect
 
 def task_list(request):
     tasks = Task.objects.all()
@@ -19,3 +21,18 @@ def task_detail(request, id):
         'todo_list/tasks/task_detail.html',
         {'task': task}
     )
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list:task_list')
+    else:
+        form = TaskCreateForm()
+    return render(
+        request,
+        'todo_list/tasks/task_create.html',
+        {'form': form}
+    )   
